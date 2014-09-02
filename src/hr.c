1bfa5ea5313c463ff771a52ed8a6e65a93b8a57f
HalosGhost <halosghost@archlinux.info>
2014-09-02 11:25:54 -0500

    Use a format to print variables

--- src/hr.c
+++ src/hr.c
@@ -15,16 +15,16 @@ main (int argc, char * argv []) {
 
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
-    int COLS = (w.ws_col <= 0 ? 80 : w.ws_col);
+    unsigned long COLS = (w.ws_col <= 0 ? 80 : w.ws_col);
 
     if ( argc <= 1 ) {
-        for ( int i = 0; i < COLS; i ++ ) {
+        for ( unsigned long i = 0; i < COLS; i ++ ) {
             printf("#");
         } printf("\n");
     } else {
         for ( int i = 1; i < argc; i ++ ) {
             for ( unsigned int j = 0; j < (COLS/strlen(argv[i])); j ++ ) {
-                printf(argv[i]);
+                printf("%s", argv[i]);
             } printf("\n");
         }
     } return 0;
