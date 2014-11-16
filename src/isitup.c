6a085c247d79282683841df41a8d951b956def89
HalosGhost <halosghost@archlinux.info>
2014-11-16 09:47:49 -0600

    Switch to argp

--- src/isitup.c
+++ src/isitup.c
@@ -11,15 +11,24 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <curl/curl.h>
-#include <getopt.h>
+#include <argp.h>
 
 // Forward Declarations //
 #define BUFFER_SIZE 120
 
-static char uri [BUFFER_SIZE] = {'\0'};
+struct args {
+    char uri [BUFFER_SIZE], response [BUFFER_SIZE];
+    bool quiet, verbose;
+};
 
-static void
-_usage (uint8_t status) __attribute__((noreturn));
+const char * argp_program_version = "isitup 1.1.0";
+const char * argp_program_bug_address = "<halosghost@archlinux.info>";
+static char * doc = "isitup -- a simple tool to check if a URI is up\v"
+                    "URI should be given as the domain name and TDL only\n"
+                    "exit codes: URI appears {0: up, 1: down, 2: invalid}";
+
+static error_t
+parse_opt (int32_t key, char * arg, struct argp_state * state);
 
 static size_t
 write_function (const char * buffer, size_t size, size_t nmemb, char * userp);
@@ -28,103 +37,88 @@ write_function (const char * buffer, size_t size, size_t nmemb, char * userp);
 int32_t
 main (int32_t argc, char * argv []) {
 
-    bool flag_quiet   = false;
-    bool flag_verbose = false;
+    struct argp_option os [] = {
+        { 0,         0,   0,     0, "Options:",           -1 },
+        { "quiet",   'q', 0,     0, "Silence all output", 0  },
+        { "verbose", 'v', 0,     0, "Output everyting",   0  },
+        { "uri",     'u', "URI", 0, "Check URI",          0  },
+        // When upstream adds support for ipv6, add relevant options
+        //{ "ipv4",    '4', 0,     0, "Resolve only ipv4",  0  },
+        //{ "ipv6",    '6', 0,     0, "Resolve only ipv6",  0  },
+        { 0,         0,   0,     0, 0,                    0  }
+    };
 
-    if ( argc <= 1 ) { 
-        _usage(1);
-    } else {
-        static struct option os [] = {              
-            /* Flags */                                  
-            { "help",     no_argument,         0, 'h' }, 
-            { "quiet",    no_argument,         0, 'q' }, 
-            { "verbose",  no_argument,         0, 'v' }, 
-            /* Switches */                               
-            { "uri",      required_argument,   0, 'u' }, 
-            { 0,          0,                   0, 0   }, 
-        };                                               
+    struct argp argp = { os, parse_opt, "", doc, NULL, NULL, 0 };
+    struct args args = { {0}, {0}, false, false };
 
-        for ( int c = 0, i = 0; c != -1;
-              c = getopt_long(argc, argv, "hqvu:", os, &i) ) {
+    argp_parse(&argp, argc, argv, 0, 0, &args);
 
-            switch ( c ) {
-                case 'h':
-                    _usage(0);
-
-                case 'q':
-                    flag_quiet   = true;
-                    flag_verbose = false;
-                    break;
-                
-                case 'u':
-                    snprintf(uri, sizeof(uri), "http://isitup.org/%s.txt", optarg);
-                    break;
-
-                case 'v':
-                    flag_verbose = true;
-                    flag_quiet   = false;
-                    break;
-            }
-        }
+    if ( !*args.uri ) {
+        fputs("No URI given\n", stderr);
+        exit(2);
     }
-
-    if ( !*uri ) _usage(1);
 
     CURL * handle = curl_easy_init();
     uint8_t status = 0;
 
     if ( handle ) {
-        char response [BUFFER_SIZE] = {'\0'};
-
-        curl_easy_setopt(handle, CURLOPT_URL, uri);
+        curl_easy_setopt(handle, CURLOPT_URL, args.uri);
         curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1);
         curl_easy_setopt(handle, CURLOPT_USERAGENT, "curl/7.35.0");
         curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_function);
-        curl_easy_setopt(handle, CURLOPT_WRITEDATA, response);
-        curl_easy_setopt(handle, CURLOPT_VERBOSE, flag_verbose);
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, args.response);
+        curl_easy_setopt(handle, CURLOPT_VERBOSE, args.verbose);
 
         if ( curl_easy_perform(handle) != CURLE_OK ) {
             curl_easy_cleanup(handle);
-            curl_global_cleanup();
             fputs("Could not reach isitup.org\n", stderr);
             exit(1);
         } else {
             int32_t port, http_response;
             double response_time;
             char ip_addr [43];
-            sscanf(response, "%*[^,], %d, %d, %[^,], %d, %lg", 
+            sscanf(args.response, "%*[^,], %d, %hhu, %[^,], %d, %lg", 
                    &port, &status, ip_addr, &http_response, &response_time);
 
-            if ( !flag_quiet ) {
+            //if ( !args.quiet ) {
                 printf("%s:%d ", ip_addr, port);
                 if ( status > 1 ) { 
                     printf("appears down\n"); 
                 } else { 
                     printf("(%d after %lgs)\n", http_response, response_time); 
                 }
-            }
+            //}
         }
     }
     
     curl_easy_cleanup(handle);
-
     return (status-1);
 }
 
 // Function Definitions //
-static void 
-_usage (uint8_t status) {
+static error_t
+parse_opt (int32_t key, char * arg, struct argp_state * state) {
 
-    fputs("Usage: isitup [-h] [-q] [-v] [-u URI]\n\n"
-          "Options:\n"
-          "  -h, --help\tprint this help and exit\n"
-          "  -q, --quiet\tprint nothing\n"
-          "  -u, --uri\tcheck status of URI\n"
-          "  -v, --verbose\tprint very verbosely\n\n"
-          "URI should be given as the domain name and TLD only\n\n"
-          "exit codes:\tURI appears {0: up, 1: down, 2: invalid}\n"
-          ,(status == 0 ? stdout : stderr));
-    exit(status);
+    struct args * args = state->input;
+    switch ( key ) {
+        case 'q':
+            args->quiet   = true;
+            args->verbose = false;
+            break;
+
+        case 'v':
+            args->quiet   = false;
+            args->verbose = true;
+            break;
+
+        case 'u':
+            snprintf(args->uri, sizeof(args->uri), 
+                     "http://isitup.org/%s.txt", arg);
+            break;
+
+        default:
+            return ARGP_ERR_UNKNOWN;
+    } return 0;
 }
 
 static size_t 
