c0e0a7c5aa657c676153a467faee0a05771172d9
halosghost <halosghost@archlinux.info>
2016-04-02 14:19:14 -0500

    Inline package list

--- cac/stage_2.sh
+++ cac/stage_2.sh
@@ -14,3 +14,13 @@ vgremove -ff centos || die 'kill LVM'
 
 msg 'Wiping partitions'
 dd bs=512 count=1 if=/dev/zero of=/dev/sda || die 'wipe /dev/sda'
+
+def_package_list=(
+   'bash' 'bzip2' 'coreutils' 'device-mapper' 'diffutils' 'e2fsprogs' 'file'
+   'filesystem' 'findutils' 'gawk' 'gcc-libs' 'gettext' 'glibc' 'grep' 'gzip'
+   'inetutils' 'iproute2' 'iputils' 'less' 'licenses' 'logrotate' 'man-db'
+   'man-pages' 'pacman' 'pciutils' 'perl' 'procps-ng' 'psmisc' 'sed' 'shadow'
+   'sysfsutils' 'systemd-sysvcompat' 'tar' 'texinfo' 'usbutils' 'util-linux'
+   'which' 'sudo' 'nftables' 'vim' 'syslinux' 'linux-grsec' 'paxd' 'gradm' 'zsh'
+   'zsh-syntax-highlighting' 'openssh'
+)
